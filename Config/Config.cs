f7a88ef2171fd51306512f78f931c911dcaa568e
PureFoxCore <icefoxtx@gmail.com>
2021-05-24 20:13:09 +0400

    Fixed All

---
+++ Config/Config.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AviFav_.Config
+{
+    public class Config
+    {
+        public static string MainFolder = "404Mods/AviFavorites";
+        public static string ModConfig = MainFolder + "/config.json";
+        public static string AvatarLists = MainFolder + "/avatars.json";
+
+        public static Config CFG;
+
+        public string CustomName = "Extended Favorites";
+        public string AddFavoriteTXT = "+Favorite";
+        public string RemoveFavoriteTXT = "-Favorite";
+        public bool Public = true;
+        public bool Custom = true;
+
+        public static List<SavedAvi> DAvatars = new List<SavedAvi> {
+
+            new SavedAvi()
+            {
+                Name = "Robot",
+                AvatarID = "avtr_c38a1615-5bf5-42b4-84eb-a8b6c37cbd11",
+                ThumbnailImageUrl = "https://d348imysud55la.cloudfront.net/Avatar-Robot-Image-2017415f1_3_a.file_0e8c4e32-7444-44ea-ade4-313c010d4bae.1.png",
+            }
+        };
+
+        public static void SaveConfig()
+        {
+            if (CFG != null)
+                File.WriteAllText(ModConfig, JsonConvert.SerializeObject(CFG, Formatting.Indented));
+        }
+        public static void UpdateAvatars()
+        {
+            if (CFG != null)
+                File.WriteAllText(AvatarLists, JsonConvert.SerializeObject(DAvatars, Formatting.Indented));
+        }
+
+        public static void LoadConfig()
+        {
+            Directory.CreateDirectory(MainFolder);
+
+            if (!File.Exists(ModConfig))
+            {
+                CFG = new Config();
+                SaveConfig();
+
+            }
+            else
+            {
+                CFG = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ModConfig));
+                if (CFG == null)
+                    CFG = new Config();
+                SaveConfig();
+            }
+            
+            if (!File.Exists(AvatarLists))
+                File.WriteAllText(AvatarLists, JsonConvert.SerializeObject(DAvatars, Formatting.Indented));
+            else
+                DAvatars = JsonConvert.DeserializeObject<List<SavedAvi>>(File.ReadAllText(AvatarLists));
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,98 @@
+﻿using AviFav_;
+using AviFav_.Config;
+using MelonLoader;
+using System;
+using System.Linq;
+using UnityEngine;
+using VRC.Core;
+
+namespace AviFavsPlus
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Avatar Favorites Plus"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "404#0004 and fixed By PureFoxCore#5212"; // Author of the Mod.  (Set as null if none)
+        public const string Company = "I am not a company -Kappa-"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.4.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/l-404-l"; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class AviFavPlus : MelonMod
+    {
+        
+        public static AvatarListApi CustomList;
+        public static AviPButton FavoriteButton;
+        public override void OnApplicationStart()
+        {
+            Config.LoadConfig(); //Needs to load the configs.
+        }
+
+        //string , GameObject , VRCSDK2.Validation.Performance.Stats.AvatarPerformanceStats
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            if (Config.CFG.Custom)
+            {
+                CustomList = AvatarListApi.Create(Config.CFG.CustomName + " / " + Config.DAvatars.Count, 1);
+                CustomList.AList.FirstLoad(Config.DAvatars);
+
+
+                CustomList.AList.field_Public_SimpleAvatarPedestal_0.field_Internal_Action_3_String_GameObject_AvatarPerformanceStats_0 = new Action<string, GameObject, VRC.SDKBase.Validation.Performance.Stats.AvatarPerformanceStats>((x, y, z) =>
+                {
+                    if (Config.DAvatars.Any(v => v.AvatarID == CustomList.AList.field_Public_SimpleAvatarPedestal_0.field_Internal_ApiAvatar_0.id))
+                    {
+                        FavoriteButton.Title.text = Config.CFG.RemoveFavoriteTXT;
+                        CustomList.ListTitle.text = Config.CFG.CustomName + " / " + Config.DAvatars.Count;
+                    }
+                    else
+                    {
+                        FavoriteButton.Title.text = Config.CFG.AddFavoriteTXT;
+                        CustomList.ListTitle.text = Config.CFG.CustomName + " / " + Config.DAvatars.Count;
+                    }
+
+                });
+
+                //Add-Remove Favorite Button
+                FavoriteButton = AviPButton.Create(Config.CFG.AddFavoriteTXT, 0f, 9.6f);
+                FavoriteButton.SetAction(() =>
+                {
+                    var avatar = CustomList.AList.field_Public_SimpleAvatarPedestal_0.field_Internal_ApiAvatar_0;
+                    if (avatar.releaseStatus != "private")
+                    {
+                        if (!Config.DAvatars.Any(v => v.AvatarID == avatar.id))
+                        {
+                            AvatarListHelper.AvatarListPassthru(avatar);
+                            CustomList.AList.Refresh(Config.DAvatars.Select(x => x.AvatarID).Reverse());
+                            FavoriteButton.Title.text = Config.CFG.RemoveFavoriteTXT;
+                            CustomList.ListTitle.text = Config.CFG.CustomName + " / " + Config.DAvatars.Count;
+                        }
+                        else
+                        {
+
+                            AvatarListHelper.AvatarListPassthru(avatar);
+                            CustomList.AList.Refresh(Config.DAvatars.Select(x => x.AvatarID).Reverse());
+                            FavoriteButton.Title.text = Config.CFG.AddFavoriteTXT;
+                            CustomList.ListTitle.text = Config.CFG.CustomName + " / " + Config.DAvatars.Count;
+                        }
+                    }
+                });
+
+                //Author Button
+                var t = AviPButton.Create("Show Author", 320f, 9.6f);
+                t.SetScale(-.1f);
+                t.SetAction(() =>
+                {
+                    VRCUiManager.prop_VRCUiManager_0.Method_Public_Void_Boolean_0(true);
+                    APIUser.FetchUser(CustomList.AList.field_Public_SimpleAvatarPedestal_0.field_Internal_ApiAvatar_0.authorId, new Action<APIUser>(x =>
+                    {
+
+                        QuickMenu.prop_QuickMenu_0.prop_APIUser_0 = x;
+                        QuickMenu.prop_QuickMenu_0.Method_Public_Void_Int32_Boolean_0(4, false);
+
+
+                    }), null);
+                });
+            }
+        }
+    }
+}
